f206a0c3a7ec9c79d72f173c4777c93376a609f1
jzonefaalam <jzone_faalam@yahoo.com>
2017-08-21 19:31:21 +0900

    Update->Schedule
    7:31PM

--- MCS/app/reservation_tbl.php
+++ MCS/app/reservation_tbl.php
@@ -9,6 +9,4 @@ class reservation_tbl extends Model
   protected $table = 'reservation_tbl';
   protected $primaryKey = 'reservationID';
   protected $fillable = array('reservationID','reservationStatus', 'eventID', 'paymentModeID','paymentTermID', 'packageID');
-  public $timestamps = false;
-  public $incrementing = false;
 }
--- MCS/app/reservationtbl.php
+++ MCS/app/reservationtbl.php
@@ -9,5 +9,4 @@ class reservationtbl extends Model
     protected $table = 'reservation_tbl';
 	protected $primaryKey = 'reservationID';
 	protected $fillable = array('reservationStatus', 'eventID', 'paymentModeID', 'paymentTermID', 'packageID');
-	public $timestamps = false;
 }
