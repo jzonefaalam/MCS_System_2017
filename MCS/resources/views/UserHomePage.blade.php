212ee323ea43c0c0fc7998c1b9030cb3648efd29
Rozhel Turgo <31346676+rozhelturgo@users.noreply.github.com>
2017-10-19 02:10:54 -0800

    qweqwe

--- MCS/resources/views/UserHomePage.blade.php
+++ MCS/resources/views/UserHomePage.blade.php
@@ -18,18 +18,26 @@
                 <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
                 <li data-target="#carousel-example-generic" data-slide-to="1"></li>
                 <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="4"></li>
               </ol>
             
               <!-- Wrapper for slides -->
               <div class="carousel-inner">
                 <div class="item active">
-                  <img src="../img/1.jpg" alt="Awesome Image">
+                  <img src="../img/5.jpg" alt="Awesome Image">
                 </div>
                 <div class="item">
                   <img src="../img/3.jpg" alt="Awesome Image">
                 </div>
                 <div class="item">
                   <img src="../img/4.jpg" alt="Awesome Image">
+                </div>
+                <div class="item">
+                  <img src="../img/6.jpg" alt="Awesome Image">
+                </div>
+                <div class="item">
+                  <img src="../img/7.jpg" alt="Awesome Image">
                 </div>
               </div>
             
